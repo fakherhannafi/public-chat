eba1c139627774a7e128224ddf2506d47914a6cf
fakhrovski <Fakhrovski@DESKTOP-LDMK5KF>
2019-01-30 00:49:57 +0100

    display last 10 messages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
 import './App.css'
 import Formulaire from './components/Formulaire';
 import Message from './components/Message'
@@ -10,16 +10,27 @@ class App extends Component {
     messages: {},
     pseudo: this.props.match.params.pseudo
   }
+  messageRef = createRef()
   componentDidMount() {
     base.syncState('/', {
       context: this,
       state: 'messages'
     })
   }
+  componentDidUpdate() {
+    const ref = this.messageRef.current
+    ref.scrollTop = ref.scrollHeight
+  }
 
   addMessage = message => {
     const messages = { ...this.state.messages }
     messages[`message-${Date.now()}`] = message
+    Object
+      .keys(messages)
+      .slice(0, -10)
+      .forEach(key => {
+        messages[key] = null
+      })
     this.setState({ messages })
   }
   render() {
@@ -34,7 +45,8 @@ class App extends Component {
     return (
       <div className='box'>
         <div>
-          <div className='messages'>
+          <div className='messages'
+            ref={this.messageRef}>
             <div className='message'>{messages}</div>
           </div>
         </div>
